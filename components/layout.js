fffad96f7d3eb3b53d22853444cd26202b60e125
Selina Schindlauer <selinaschindlauer@gmail.com>
2021-06-14 19:41:56 +0200

    site title + favicon

--- components/layout.js
+++ components/layout.js
@@ -14,7 +14,8 @@ export default function Layout({children, home, about, projects}) {
     return (
         <div>
             <Head>
-                <link rel="icon" href="/favicon.ico"/>
+                <link rel="icon" href="../images/favicon.png"/>
+                <title>{siteTitle}</title>
                 <meta
                     name="description"
                     content="Portfolio UI/UX und Web Development"
@@ -26,7 +27,7 @@ export default function Layout({children, home, about, projects}) {
                         siteTitle
                     )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
                 />
-                <meta name="og:title" content={siteTitle}/>
+                <meta name="Selina Julia" content="Selina Julia"/>
                 <meta name="twitter:card" content="summary_large_image"/>
                 <link rel="preconnect" href="https://fonts.gstatic.com"/>
                 <link
